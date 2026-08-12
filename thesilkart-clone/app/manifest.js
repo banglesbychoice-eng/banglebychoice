a37d8b9f7f4ba8ba0aa6ab8be36f390a2662d89c
Bangles By Choice <banglesbychoice@gmail.com>
2026-08-12 13:30:27 +0530

    Use supplied brand artwork for favicon

--- thesilkart-clone/app/manifest.js
+++ thesilkart-clone/app/manifest.js
@@ -3,12 +3,12 @@ export default function manifest() {
     name: 'Bangle by Choice', short_name: 'BBC', description: 'Premium kundan stones, silk thread and jewellery-making materials delivered across India.',
     id: '/', start_url: '/', scope: '/', display: 'standalone', orientation: 'portrait', background_color: '#fff9f6', theme_color: '#6e1734',
     icons: [
-      { src: '/favicon.ico', sizes: 'any', type: 'image/x-icon' },
-      { src: '/icons/icon-48.png', sizes: '48x48', type: 'image/png', purpose: 'any' },
-      { src: '/icons/icon-96.png', sizes: '96x96', type: 'image/png', purpose: 'any' },
-      { src: '/icons/icon-180.png', sizes: '180x180', type: 'image/png', purpose: 'any' },
-      { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
-      { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
+      { src: '/favicon.ico?v=20260812', sizes: 'any', type: 'image/x-icon' },
+      { src: '/icons/icon-48.png?v=20260812', sizes: '48x48', type: 'image/png', purpose: 'any' },
+      { src: '/icons/icon-96.png?v=20260812', sizes: '96x96', type: 'image/png', purpose: 'any' },
+      { src: '/icons/icon-180.png?v=20260812', sizes: '180x180', type: 'image/png', purpose: 'any' },
+      { src: '/icons/icon-192.png?v=20260812', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
+      { src: '/icons/icon-512.png?v=20260812', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
     ],
     categories: ['shopping', 'lifestyle', 'business'],
     shortcuts: [
